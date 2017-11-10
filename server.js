c810491e0dc5549e090d936774d4175c68514ca7
Nagappan <n4nagappan@gmail.com>
2017-11-10 11:40:03 -0500

    Add simple tests with mocha

--- server.js
+++ server.js
@@ -3,4 +3,5 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3333, () => console.log('Example app listening on port 3333!'))
+server = app.listen(3333, () => console.log('Example app listening on port 3333!'))
+module.exports = server;
